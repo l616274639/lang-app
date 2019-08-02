1a4487598fd18f701705692f79d1d2800a73ad1f
l616274639 <616274639@qq.com>
2019-08-02 13:58:45 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,26 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import './common/uni.css';
+	@import './common/common.css';
+	
+	page {
+		background-color: #F4F5F6;
+		height: 100%;
+		font-size: 28upx;
+		line-height: 1.8;
+	}
+</style>
---
+++ main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App'
+import VueI18n from './common/js/vue-i18n.min' 
+
+Vue.use(VueI18n)
+Vue.config.productionTip = false
+
+const i18n = new VueI18n({  
+  locale: 'zh',  
+  messages: {  
+    'en': require('./common/lang/en'), 
+    'zh': require('./common/lang/zh')
+  }  
+}) 
+
+Vue.prototype._i18n = i18n 
+App.mpType = 'app'
+
+const app = new Vue({
+	i18n,
+    ...App
+})
+
+app.$mount()

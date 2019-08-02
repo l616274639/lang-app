3149bb88b1fcf6a2c4f56041bb9c730f733ea037
l616274639 <616274639@qq.com>
2019-08-02 14:09:20 +0800

    Add files via upload

---
+++ common/lang/en.js
@@ -0,0 +1,35 @@
+module.exports = {
+    label: {
+		account: 'Account',
+    	rememberAccount: 'Remember Account',
+		forgetPassword: 'Forget Password',
+		setting: 'Setting',
+		other: 'Other',
+		aboutSystem: 'About System',
+		faq: 'FAQ',
+		qrCode: 'APP QR Code',
+		parts: 'Parts',
+		SD: 'SD',
+		trade: 'Trade'
+    },
+	button: {
+	    login: 'Login',
+		logout: 'Logout'
+	},
+    title: {
+        login: 'Login',
+		homePage: 'Home Page',
+		message: 'Message',
+		setting: 'Setting',
+		changeLanguage: 'Change Language',
+		changePassword: 'Change Password',
+		changeEmail: 'Change Email',
+		aboutSystem: 'About System',
+		faq: 'FAQ',
+		qrCode: 'APP QR Code'
+    },
+    placeholder: {
+		accountCd: 'Please input account',
+		password: 'Please input password',
+    }
+}
---
+++ common/lang/zh.js
@@ -0,0 +1,35 @@
+module.exports = {
+    label: {
+		account: '账号',
+    	rememberAccount: '记住用户名',
+		forgetPassword: '忘记密码',
+		setting: '设置',
+		other: '其他',
+		aboutSystem: '关于系统',
+		faq: '常见问题',
+		qrCode: '系统安装二维码',
+		parts: '配件',
+		SD: '整机',
+		trade: '交易'
+    },
+	button: {
+	    login: '登录',
+		logout: '退出登录',
+	},
+    title: {
+        login: '登录',
+		homePage: '首页',
+		message: '消息',
+		setting: '设置',
+		changeLanguage: '语言变更',
+		changePassword: '密码变更',
+		changeEmail: '邮箱变更',
+		aboutSystem: '关于系统',
+		faq: '常见问题',
+		qrCode: '系统下载二维码'
+    },
+    placeholder: {
+		accountCd: '请输入用户名',
+		password: '请输入密码',
+    }
+}
